8b767b33e06ad5d96b3081acf46944fa8c5c5952
Maiko Duarte <mkdu185@gmail.com>
2023-11-09 18:19:08 -0300

    feat: add mvc agencia de carros

---
+++ mjv-aula-10/Models/Carro.cs
@@ -0,0 +1,11 @@
+﻿namespace mjv_aula_10.Models;
+
+public class Carro
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+    public string Cor { get; set; }
+    
+    
+}
---
+++ mjv-aula-10/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mjv_aula_10.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
