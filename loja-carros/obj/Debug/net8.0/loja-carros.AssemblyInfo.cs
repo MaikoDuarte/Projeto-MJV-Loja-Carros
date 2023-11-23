19bb2e944506f27eb30eef95b5db994e6afb5805
Maiko Duarte <mkdu185@gmail.com>
2023-11-22 21:14:35 -0300

    style: adicionando estilização dos buttons e navbar

--- loja-carros/obj/Debug/net8.0/loja-carros.AssemblyInfo.cs
+++ loja-carros/obj/Debug/net8.0/loja-carros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("loja-carros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2642cf6f2f0ca5183d268ad1a6bc2af2d182d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("loja-carros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("loja-carros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
