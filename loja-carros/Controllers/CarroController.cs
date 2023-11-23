9d4f5e6ea3d595258077d335a7ce2f495446403c
Maiko Duarte <mkdu185@gmail.com>
2023-11-23 12:52:37 -0300

    style: estilizando formularios e botoes

--- loja-carros/Controllers/CarroController.cs
+++ loja-carros/Controllers/CarroController.cs
@@ -6,6 +6,7 @@ using System;
 public class CarroController : Controller
 {
     private readonly PostgresDAL _postgresDal;
+    
 
     public CarroController(PostgresDAL postgresDal)
     {
--- loja-carros/Program.cs
+++ loja-carros/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connString = builder.Configuration.GetConnectionString("conn_postgres");
+var tableName = builder.Configuration["Database:table_name"];
 builder.Services.AddScoped<PostgresDAL>();
 
 var app = builder.Build();
